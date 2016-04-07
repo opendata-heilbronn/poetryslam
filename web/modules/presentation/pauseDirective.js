b53c1c8378be8fefaeffbb904629361e7c5cf5dd
Felix Ebert <felix.ebert@if-core.de>
2016-04-07 12:50:45 +0200

    show different screens depending on variable "screen" in event

---
+++ web/modules/presentation/pauseDirective.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+    angular.module('ps.presentation').directive('intro', function () {
+        return {
+            restrict: 'E',
+            templateUrl: '/modules/presentation/partials/intro.html'
+        }
+    });
+})();
--- web/modules/ps.js
+++ web/modules/ps.js
@@ -1,5 +1,5 @@
 (function () {
     'use strict';
 
-    angular.module('ps', ['ps.sync']);
+    angular.module('ps', ['ps.sync', 'ps.presentation']);
 })();
