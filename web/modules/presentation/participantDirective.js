5e54bfd7d57db2a2e5fc268b67df9b4f143f9992
Jonathan Günz <jonathan.guenz@outlook.com>
2016-04-08 20:35:45 +0200

    Trello #1 und #2 - Animationen in Teilnehmeranzeige bei der Anzeige der Punkte
    Listen: Nhyx - Machines (https://soundcloud.com/nhyx/machines)

--- web/modules/presentation/participantDirective.js
+++ web/modules/presentation/participantDirective.js
@@ -4,42 +4,50 @@
         return {
             restrict: 'E',
             templateUrl: '/modules/presentation/partials/participant.html',
-            link: function() {
-                window.setTimeout(function() {
-                    $(".fitInside").each(function(index, elem) {
-                        var content = $(elem).text();
-                        $(elem).text("abc");
-                        var maxHeight = $(elem).height();
-                        $(elem).text(content);
+            controller: function($rootScope) {
 
-                        while ($(elem).height() > maxHeight - 20) {
-                            $(elem).css(
-                                "font-size",
-                                (parseInt($(".fitInside").css("font-size").replace("px", ""), 10) - 2) + "px");
+                $rootScope.$watch("event", function() {
+                    console.log("event changed");
+
+                    window.setTimeout(function() {
+                        $("[max-lines]").each(function(index, elem) {
+                            var content = $(elem).text();
+                            $(elem).text("abc");
+                            var maxHeight = $(elem).height();
+                            $(elem).text(content);
+
+                            if ($rootScope.event.phase == 'name') {
+                                $(elem).removeClass("overlay-highlight")
+                                    .addClass("overlay-highlight-long-text-phase-name");
+                            } else {
+                                $(elem).removeClass("overlay-highlight")
+                                    .addClass("overlay-highlight-long-text-phase-rating");
+                            }
+                        });
+                    }, 500);
+
+
+                    window.setTimeout(function() {
+                        $(".delay-5s").removeClass("delay-5s");
+                    }, 10000);
+
+                    window.setTimeout(function() {
+                        if (document.querySelector(".to-overlay-subline") !== null &&
+                            document.querySelector(".to-overlay-subline").classList !== null) {
+
+                            document.querySelector(".to-overlay-subline")
+                                .classList
+                                .remove("overlay-highlight");
+                            document.querySelector(".to-overlay-subline")
+                                .classList
+                                .add("overlay-subline");
+                            document.querySelector(".to-overlay-subline")
+                                .classList
+                                .add("overlay-subline-with-margin-top");
                         }
-                    });
-                }, 1000);
-
-
-                window.setTimeout(function() {
-                    $(".delay-5s").removeClass("delay-5s");
-                }, 10000);
-
-                window.setTimeout(function() {
-                    if (document.querySelector(".to-overlay-subline") !== null && document.querySelector(".to-overlay-subline").classList !== null) {
-                        document.querySelector(".to-overlay-subline")
-                            .classList
-                            .remove("overlay-highlight");
-                        document.querySelector(".to-overlay-subline")
-                            .classList
-                            .add("overlay-subline");
-                        document.querySelector(".to-overlay-subline")
-                            .classList
-                            .add("overlay-subline-with-margin-top");
-                    }
-                }, 5000);
+                    }, 5000);
+                });
             }
-
         };
     });
 })();
