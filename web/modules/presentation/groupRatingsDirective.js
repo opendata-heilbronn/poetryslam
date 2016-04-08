dfa076664d45c8910b757d930782adfc1f990f07
Jonathan Günz <jonathan.guenz@outlook.com>
2016-04-08 21:27:17 +0200

    Trello #6 - Anzeige der Wertungen in der Gruppenansicht nach einander
    Listen: Space Raptor - Holy Garden (https://soundcloud.com/spaceraptorband/holy-garden?in=spaceraptorband/sets/hyperion)

--- web/modules/presentation/groupRatingsDirective.js
+++ web/modules/presentation/groupRatingsDirective.js
@@ -1,9 +1,27 @@
-(function () {
+(function() {
     'use strict';
-    angular.module('ps.presentation').directive('groupRatings', function () {
+    angular.module('ps.presentation').directive('groupRatings', function() {
         return {
             restrict: 'E',
-            templateUrl: '/modules/presentation/partials/groupRatings.html'
-        }
+            templateUrl: '/modules/presentation/partials/groupRatings.html',
+            controller: function($rootScope, $scope) {
+                $scope.participants = [];
+
+                $scope.next = function() {
+                    $scope.participants.push(
+                        $rootScope.event.participants[
+                        $scope.participants.length
+                        ]
+                    );
+
+                    if ($scope.participants.length < $rootScope.event.participants.length) {
+                        window.setTimeout($scope.next, 1000);
+                        $scope.$apply();
+                    }
+                };
+
+                window.setTimeout($scope.next, 500);
+            }
+        };
     });
 })();
