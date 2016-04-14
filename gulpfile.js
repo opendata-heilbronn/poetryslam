bb7a8aa9dccd37fd04eeb8491aedf7612cac17d3
Felix Ebert <felix.ebert@if-core.de>
2016-04-14 07:50:54 +0200

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var del = require('del');
+
+/**
+ * build tasks
+ */
+gulp.task('clean', function (cb) {
+    del(['build/**'], cb);
+});
+gulp.task('copy-images', ['clean'], function () {
+    return gulp.src('src/media/**/*')
+        .pipe(gulp.dest('build/media'));
+});
+gulp.task('copy-fonts', ['clean'], function () {
+    return gulp.src('src/fonts/**')
+        .pipe(gulp.dest('build/fonts'));
+});
+gulp.task('copy-icons', ['clean'], function () {
+    return gulp.src('src/material-icons/**')
+        .pipe(gulp.dest('build/material-icons'));
+});
+gulp.task('copy-modules', ['clean'], function () {
+    return gulp.src('src/modules/**/*')
+        .pipe(gulp.dest('build/modules'));
+});
+gulp.task('copy-components', ['clean'], function () {
+    return gulp.src('src/components/**/*.{js,css}')
+        .pipe(gulp.dest('build/components'));
+});
+gulp.task('copy-html', ['clean'], function () {
+    return gulp.src('src/*.html')
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('build', ['copy-images', 'copy-fonts', 'copy-icons', 'copy-modules', 'copy-components', 'copy-html']);
