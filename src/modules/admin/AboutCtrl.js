a8a16cbd4939c24c7295cb1438e1061c561fbe25
Felix Ebert <felix.ebert@if-core.de>
2016-04-18 12:59:28 +0200

    version 1.0.6

--- src/modules/admin/AboutCtrl.js
+++ src/modules/admin/AboutCtrl.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    var currentVersion = '1.0.5';
+    var currentVersion = '1.0.6';
 
     var getAppCacheStatus = function () {
         var appCache = window.applicationCache;
