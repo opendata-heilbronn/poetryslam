0a8f8449444b05e235c4e1b0b8c0b83391b2bc06
mg166@hdm-stuttgart.de <mg166@hdm-stuttgart.de>
2016-06-23 21:13:17 +0200

    created template service and directive for local storage service

---
+++ src/modules/admin/Export_Import/downloadLocalStorageDirective.js
@@ -0,0 +1,20 @@
+/**
+ * Created by moritz on 23/06/16.
+ */
+
+(function () {
+    'use strict';
+    angular.module('psadmin').directive('downloadLocalStorage', function ($localStorage) {
+        return {
+            restrict: 'E',
+            scope: true,
+            template: '<button ng-click="click()">Click me</button> Clicked {{clicked}} times',
+            controller: function($scope){
+                $scope.clicked = 0;
+                $scope.click = function(){
+                    $scope.clicked++
+                }
+            }
+        }
+    });
+})();
---
+++ src/modules/admin/Export_Import/storageService.js
@@ -0,0 +1,11 @@
+(function () {
+    'use strict';
+
+    angular.module('ps.admin').service('storageService', function () {
+        this.getWholeLocalStorage = function () {
+
+            return "testasdfasdf";
+        };
+        return this;
+    });
+})();
