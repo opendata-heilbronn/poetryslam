a45ea3ed3a797408fb06dd0b99934f6365f3d0ad
Jan Kaltofen <Jan@Kaltofen.eu>
2016-07-21 21:35:07 +0200

    use md-confirm-dialog instead of the selfmade dialog

--- src/modules/admin/deleteButtonDirective.js
+++ src/modules/admin/deleteButtonDirective.js
@@ -9,23 +9,22 @@
             },
             templateUrl: '/modules/admin/partials/deleteButton.html',
             link: function (scope, element, attrs) {
-              scope.checkDelete = function() {
-                $mdDialog.show({
-                    templateUrl: '/modules/admin/partials/dialogs/deleteDialog.html',
-                    controller: function ($scope, $mdDialog) {
-                        $scope.delete = function () {
-                          $mdDialog.hide();
-                          if(typeof scope.list !== 'undefined') {
-                            scope.list.splice(scope.list.indexOf(scope.entry), 1);
-                          } else {
-                            scope.entry = null;
-                          }
-                        };
-                        $scope.cancel = function () {
-                          $mdDialog.hide();
-                        };
-                    }
-                });
+              scope.checkDelete = function(ev) {
+                var confirm = $mdDialog.confirm()
+                      .title('Wollen Sie den Eintrag wirklich Löschen?')
+                      .textContent('Weg ist weg, das kommt nimmer wieder!')
+                      .ariaLabel('Löschen')
+                      .targetEvent(ev)
+                      .ok('Löschen')
+                      .cancel('Abbrechen');
+
+                $mdDialog.show(confirm).then(function() {
+                  if(typeof scope.list !== 'undefined') {
+                    scope.list.splice(scope.list.indexOf(scope.entry), 1);
+                  } else {
+                    scope.entry = null;
+                  }
+                }, function() {});
               };
             }
         }
