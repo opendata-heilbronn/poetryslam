8bde2d6bcf699e710e6f8355977f60b5a0992c3e
mg166 <mg166@hdm-stuttgart.de>
2016-08-20 20:30:33 +0200

    finished sound pad

--- src/modules/admin/SoundPadDirective.js
+++ src/modules/admin/SoundPadDirective.js
@@ -8,60 +8,40 @@
             restrict: 'E',
             scope: true,
             template: '<audio controls id="media-audio"></audio>' +
-                '<div layout="row" layout-wrap> <div ng-repeat="file in soundFileList"><div flex="33">'
-            +'<md-button ng-class="{\'md-raised\':file.active == true}" ng-click="toggle($index)">{{soundFileList[$index].title}}</md-button>'
-            +'</div></div></div><md-button ng-click="stop()"><md-icon md-svg-icon="av:stop"></md-icon></md-button>' +
-            '<md-progress-linear id="progress-bar" md-mode="determinate" value="{{determinateValue}}"></md-progress-linear>'+
+            '<div layout="row" layout-wrap> <div ng-repeat="file in soundFileList track by $index"><div flex="33">'
+            + '<md-button ng-class="{\'md-raised\':file.active == true}" ng-click="toggle($index)">{{soundFileList[$index].name}}</md-button>'
+            + '</div></div></div><md-button ng-click="stop()"><md-icon md-svg-icon="av:stop"></md-icon></md-button>' +
+            '<md-progress-linear id="progress-bar" md-mode="determinate" value="{{determinateValue}}"></md-progress-linear>' +
             '<md-button ng-click="log()">sadfsadfsadf</md-button>',
             link: function (scope) {
                 var mediaPlayer;
-                    mediaPlayer = document.getElementById('media-audio');
-                    mediaPlayer.controls = false;
-                //link with rootscope object here
-                scope.soundFileList = [];
+                mediaPlayer = document.getElementById('media-audio');
+                mediaPlayer.controls = false;
 
-                //strike this code when the root scope works
-                FileStorage.getAllAudio().then(function (res) {
-                    for (i= 0; i < res.length; i++){
-                        scope.soundFileList.push({'src': res[i].objectUrl,'title':res[i].file.name});
-                    }
-                });
-
-                var i;
-
+                scope.soundFileList = scope.$root.event.files;
+                //play or stop function
                 scope.toggle = function (index) {
                     var i;
-                    for(i=0; i < scope.soundFileList.length ;i++){
+                    for (i = 0; i < scope.soundFileList.length; i++) {
                         scope.soundFileList[i].active = false;
                     }
-                        mediaPlayer.currentTime = 0;
-                        mediaPlayer.src = scope.soundFileList[index].src;
-                        scope.soundFileList[index].active = true;
-                        mediaPlayer.load();
-                        mediaPlayer.play();
+                    mediaPlayer.currentTime = 0;
+                    mediaPlayer.src = scope.soundFileList[index].url;
+                    scope.soundFileList[index].active = true;
+                    mediaPlayer.load();
+                    mediaPlayer.play();
                 };
-                //md-mode="determinate" value="{{determinateValue}}"
                 mediaPlayer.addEventListener('timeupdate', updateProgressBar, false);
                 function updateProgressBar() {
-                    var progressBar = document.getElementById('progress-bar');
-                    var percentage = Math.floor((100 / mediaPlayer.duration) *
+                    scope.determinateValue = Math.floor((100 / mediaPlayer.duration) *
                         mediaPlayer.currentTime);
-                    scope.determinateValue = percentage;
-                    console.log(mediaPlayer.currentTime);
                     scope.$apply();
-                    progressBar.value = percentage;
-                    //progressBar.innerHTML = percentage + '% played';
                 }
-                scope.stop = function (){
+
+                scope.stop = function () {
                     mediaPlayer.pause();
                     mediaPlayer.currentTime = 0;
                 };
-                scope.log = function () {
-                    //console.log(JSON.stringify(FileStorage.get()));
-                    FileStorage.getAllAudio().then(function (res) {
-                        console.log(JSON.stringify(res[0].file.name)+" #");
-                    })
-                }
             }
         }
     });
