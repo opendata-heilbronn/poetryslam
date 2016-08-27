f612dcd5d4e0d19927cd251d9fa1a3ce74d37b97
Jonathan Günz <jonathan.guenz@outlook.com>
2016-08-27 14:15:56 +0200

    start miaximized

--- src/background.js
+++ src/background.js
@@ -1,8 +1,5 @@
 chrome.app.runtime.onLaunched.addListener(function () {
     chrome.app.window.create('admin.html', {
-        'outerBounds': {
-            'width': 400,
-            'height': 500
-        }
+        'state': 'maximized'
     });
 });
