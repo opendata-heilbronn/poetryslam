cdd130e1519b49854da7c54e4c5badd66494acd8
Jan Kaltofen <Jan@Kaltofen.eu>
2016-09-07 22:01:30 +0200

    revised participant-tab

--- src/modules/admin/participantsTabDirective.js
+++ src/modules/admin/participantsTabDirective.js
@@ -4,7 +4,19 @@
         return {
             restrict: 'E',
             scope: false,
-            templateUrl: '/modules/admin/partials/participantsTab.html'
+            templateUrl: '/modules/admin/partials/participantsTab.html',
+            controller: function($scope, $rootScope) {
+              $scope.participantIsSelected = function(participant) {
+                if(participant.id == $rootScope.event.view.participantId) {
+                  return true;
+                } else {
+                  return false;
+                }
+              };
+              $scope.selectParticipant = function(participant) {
+                $rootScope.event.view.participantId = participant.id;
+              };
+            }
         }
     });
 })();
