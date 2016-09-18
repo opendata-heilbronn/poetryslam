7b954cbb09955cb168b8f182b302230822cb42e0
Felix Ebert <felix.ebert@if-core.de>
2016-09-18 04:49:05 +0200

    increase version

--- src/modules/admin/AboutCtrl.js
+++ src/modules/admin/AboutCtrl.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    var currentVersion = '1.0.7';
+    var currentVersion = '1.1.0';
 
     var getAppCacheStatus = function () {
         var appCache = window.applicationCache;
