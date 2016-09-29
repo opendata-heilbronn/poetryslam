c9e48b918d9d068063693f60c004db7ef061abc6
mg166 <mg166@hdm-stuttgart.de>
2016-09-29 22:05:54 +0200

    sound works now, added play/button with symbol and fuctionality

--- src/modules/admin/mediaPanelDirective.js
+++ src/modules/admin/mediaPanelDirective.js
@@ -9,6 +9,14 @@
                 scope.event = scope.$root.event;
                 scope.setScreen = scope.$parent.setScreen;
                 scope.selected = {};
+                scope.audiomode = {};
+                scope.audiomode.playing = false;
+                scope.audiomode.symbol = "play_arrow";
+                scope.audiomode.time = 0;
+                scope.audiomode.lastIndex = 31000;
+
+                // mediaPlayer = document.getElementById('media-audio');
+                // mediaPlayer.controls = false;
 
                 // Movies
                 scope.playBgVideo = function (type) {
@@ -47,53 +55,96 @@
                         mediaPlayer.currentTime);
                     scope.$apply();
                 };
-                scope.playSound = function (event, sound) {
-                    var element = getAudioElement();
-                    scope.selectedAudio = sound;
-                    FileService.getObjectUrl(sound.id).then(function(objectUrl) {
-                        element.currentTime = 0;
-                        element.src = objectUrl;
-                        element.load();
-                        element.volume = 0.0;
-                        var fadePoint = 0;
+                scope.playSound = function (event, sound, index) {
+                    if (scope.audiomode.playing == true && index == scope.audiomode.lastIndex) {
+                        scope.audiomode.playing = false;
+                        scope.audiomode.symbol = "play_arrow";
+                        scope.audiomode.time = scope.element.currentTime;
+                        // scope.audiomode.time = scope.element.currentTime;
+                        scope.element.volume = 0;
+                        setTimeout(function () {
+                            scope.element.pause();
+                        },2);
+
+                    } else {
+                        scope.audiomode.symbol = "pause";
+                        scope.audiomode.playing = true;
+                        if(index == scope.audiomode.lastIndex){
+                            console.log("index is the same");
+                            playWithFadeIn(scope.audiomode.time);
+                        }else {
+                            scope.audiomode.lastIndex = index;
+                            scope.element = getAudioElement();
+                            scope.selectedAudio = sound;
+                            FileService.getObjectUrl(sound.id).then(function (objectUrl) {
+                                scope.element.currentTime = 0;
+                                scope.element.src = objectUrl;
+                                playWithFadeIn(0);
+                            });
+                        }
+
+                    }
+                    function playWithFadeIn(timeToStart) {
+                        scope.element.load();
+                        scope.element.volume = 0.0;
+                        var fadePoint = 99999;
                         //the duration (metadata) takes longer to load then the sound
-                        element.onloadedmetadata = function () {
-                            fadePoint = element.duration - 0.01;
+                        scope.element.onloadedmetadata = function () {
+                            fadePoint = scope.element.duration - 0.01;
                         };
-                        element.play();
+                        try {
+                            scope.element.currentTime = timeToStart;
+                        }
+                        catch(err) {
+                            console.log(err);
+                        }
+                        scope.element.play();
 
                         var fadeAudioStart = setInterval(function () {
+                            console.log("in interval start");
 
                             // Only fade if past the fade out point or not at zero already
 
-                            if ((element.currentTime <= 0.01) && (element.volume !== 1.0)) {
+                            if ((scope.element.currentTime <= 0.02+timeToStart) && (scope.element.volume !== 1.0)) {
                                 //multip. for parabel fade
-                                element.volume = 1;
+                                console.log("in if from start");
+
+                                scope.element.volume = 1;
                             }
                             // When volume at zero stop all the intervalling
-                            if (element.volume === 1.0) {
+                            if (scope.element.volume === 1.0) {
                                 clearInterval(fadeAudioStart);
                             }
                         }, 1);
                         // Set the point in playback that fadeout begins. This is for a 2 second fade out.
-                        var fadeAudioEnd = setInterval(function () {
+                        var detectCloseToEnd = setInterval(function () {
+                            if(scope.element.currentTime >= (fadePoint - 1.1) || scope.element.volume == 0) {
+                                var fadeAudioEnd = setInterval(function () {
 
-                            // Only fade if past the fade out point or not at zero already
-                            if ((element.currentTime >= fadePoint) && (element.volume != 0.0)) {
-                                element.volume = 0;
-                            }
-                            // When volume at zero stop all the intervalling
-                            if (element.volume <= 0.0 && element.currentTime > fadePoint) {
-                                console.log("clear interval  out");
+                                    // Only fade if past the fade out point or not at zero already
+                                    if ((scope.element.currentTime >= fadePoint) && (scope.element.volume != 0.0)) {
+                                        scope.element.volume = 0;
+                                        scope.audiomode.time = 0;
+                                    }
+                                    // When volume at zero stop all the intervalling
+                                    if (scope.element.volume <= 0.0) {
+                                        scope.audiomode.playing = false;
+                                        scope.audiomode.symbol = "play_arrow";
+                                        clearInterval(detectCloseToEnd);
+                                        clearInterval(fadeAudioEnd);
 
-                                clearInterval(fadeAudioEnd);
+                                    }
+                                }, 1);
                             }
-                        }, 200);
-                    });
+                        }, 1000);
+                    }
                 };
                 scope.stopSound = function (event) {
                     var element = getAudioElement();
-                    element.pause();
+                    element.volume = 0;
+                    setTimeout(function () {
+                        element.pause();
+                    },1);
                 };
             }
         };
