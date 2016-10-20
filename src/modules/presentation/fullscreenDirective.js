9d0fa2cb1715f9b3b3f89d6e5c872f2f68bc9b1b
Felix Ebert <felix.ebert@if-core.de>
2016-10-20 23:23:41 +0200

    hide fullscreen request when preview is only embedded or used in the webversion

--- src/modules/presentation/fullscreenDirective.js
+++ src/modules/presentation/fullscreenDirective.js
@@ -1,22 +1,21 @@
 (function () {
-        'use strict';
-        angular.module('ps.presentation').directive('fullscreen', function ($mdDialog) {
-                return {
-                        restrict: 'A',
-                        link: function (scope, element, attrs) {
+    'use strict';
+    angular.module('ps.presentation').directive('fullscreen', function ($mdDialog, env) {
+        return {
+            restrict: 'A',
+            link: function () {
+                if (env.runtime !== 'chrome' || (window.location.search && window.location.search.indexOf('embedded=1') >= 0)) return true;
 
-                                var confirm = $mdDialog.confirm()
-                                        .title('Vollbild')
-                                        .textContent('Vollbildmodus anschalten.')
-                                        .ok('Vollbild')
-                                        .cancel('Fenstermodus');
+                var confirm = $mdDialog.confirm()
+                    .title('Vollbild')
+                    .textContent('Vollbildmodus anschalten?')
+                    .ok('Vollbild')
+                    .cancel('Fenstermodus');
 
-                                $mdDialog.show(confirm).then(function () {
-                                        chrome.app.window.current().fullscreen();
-                                }, function () {
-                                        
-                                });
-                        }
-                };
-        });
+                $mdDialog.show(confirm).then(function () {
+                    chrome.app.window.current().fullscreen();
+                });
+            }
+        };
+    });
 })();
